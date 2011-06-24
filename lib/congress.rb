4f52ff159ea0d648e5337b59da428f7164d95571
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 18:47:20 -0700

    Wrap all module methods in class << self

--- lib/congress.rb
+++ lib/congress.rb
@@ -6,22 +6,22 @@ module Congress
     def configure
       yield self
     end
-  end
 
-  # Alias for Congress::Client.new
-  #
-  # @return [Congress::Client]
-  def self.new
-    Congress::Client.new
-  end
+    # Alias for Congress::Client.new
+    #
+    # @return [Congress::Client]
+    def new
+      Congress::Client.new
+    end
 
-  # Delegate to Congress::Client
-  def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
-  end
+    # Delegate to Congress::Client
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
 
-  def self.respond_to?(method, include_private=false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
 end
