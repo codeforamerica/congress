8fd00c37b6d8877c852cfc5d041c290f0ac58b9e
Guillermo Pascual <akira.space@gmail.com>
2014-01-06 05:37:18 +0100

    Some refactors.

--- lib/congress/connection.rb
+++ lib/congress/connection.rb
@@ -2,18 +2,26 @@ require 'faraday_middleware'
 
 module Congress
   module Connection
-    ENDPOINT = 'https://congress.api.sunlightfoundation.com'
+    ENDPOINT = 'https://congress.api.sunlightfoundation.com'.freeze
 
   private
 
     def connection
-      @connection ||= Faraday.new(:url => ENDPOINT) do |connection|
-        connection.use Faraday::Request::UrlEncoded
-        connection.use Faraday::Response::RaiseError
-        connection.use Faraday::Response::Rashify
-        connection.use Faraday::Response::ParseJson
+      @connection ||= create_connection
+    end
+
+    def create_connection
+      Faraday.new(:url => ENDPOINT) do |connection|
+        middlewares.each { |middleware| connection.use(middleware) }
         connection.adapter(Faraday.default_adapter)
       end
+    end
+
+    def middlewares
+      [Faraday::Request::UrlEncoded,
+       Faraday::Response::RaiseError,
+       Faraday::Response::Rashify,
+       Faraday::Response::ParseJson]
     end
   end
 end
