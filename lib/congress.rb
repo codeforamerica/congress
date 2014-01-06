d5c40bfbd25839b3be44c0d7d09b9d5c4a3cb81a
Guillermo Pascual <akira.space@gmail.com>
2014-01-06 01:45:07 +0100

    Refactor “class << self” to “extend self” in module Congress.

--- lib/congress.rb
+++ lib/congress.rb
@@ -1,27 +1,27 @@
 require 'congress/client'
 
 module Congress
-  class << self
-    attr_accessor :key
-    def configure
-      yield self
-    end
+  extend self # rubocop:disable ModuleFunction
+  attr_accessor :key
 
-    # Alias for Congress::Client.new
-    #
-    # @return [Congress::Client]
-    def new
-      Congress::Client.new
-    end
+  def configure
+    yield self
+  end
 
-    # Delegate to Congress::Client
-    def method_missing(method, *args, &block)
-      return super unless new.respond_to?(method)
-      new.send(method, *args, &block)
-    end
+  # Alias for Congress::Client.new
+  #
+  # @return [Congress::Client]
+  def new
+    Congress::Client.new
+  end
 
-    def respond_to?(method, include_private = false)
-      new.respond_to?(method, include_private) || super(method, include_private)
-    end
+  # Delegate to Congress::Client
+  def method_missing(method, *args, &block)
+    return super unless new.respond_to?(method)
+    new.send(method, *args, &block)
+  end
+
+  def respond_to?(method, include_private = false)
+    new.respond_to?(method, include_private) || super(method, include_private)
   end
 end
