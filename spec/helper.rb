48f6cf93b770cf531f84e00b5373d302842b10ab
Erik Michaels-Ober <sferik@gmail.com>
2014-01-07 18:09:00 +0100

    Enforce use of expect syntax (no "should"s)

--- spec/helper.rb
+++ spec/helper.rb
@@ -15,6 +15,12 @@ require 'congress'
 require 'rspec'
 require 'webmock/rspec'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 WebMock.disable_net_connect!(:allow => 'coveralls.io')
 
 def a_get(path)
